d165b2c1aa0f6a5606ee42a6af2273533c315875
ashwinhubli <66745007+ashwinhubli@users.noreply.github.com>
2022-11-19 14:15:17 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -7,22 +7,47 @@ var peer = new Peer(undefined, {
 });
 
 const user = prompt("Enter your name");
-const myVideo = document.createElement("video")
-myVideo.muted = true
-let myStream
-navigator.mediaDevices 
-.getUserMedia({audio:true,video:true}).then((stream)=>{
-    myStream = stream
-    addVideoStream(myVideo,stream)
-})
 
-function addVideoStream(video,stream){
-    video.srcObject = stream
-    video.addEventListener("loadedmetadata",()=>{
-        video.play()
-        $("#video_grid").append(video)
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+let myStream;
+
+navigator.mediaDevices
+    .getUserMedia({
+        audio: true,
+        video: true,
+    })
+    .then((stream) => {
+        myStream = stream;
+        addVideoStream(myVideo, stream);
+        socket.on("user-connected",(userId)=>{
+            connectToNewUser(userId,stream)
+        })
+        peer.on("call",(call)=>{
+            call.answer(stream)
+            const video = document.createElement("video")
+            call.on("stream",(userVideoStream)=>{
+                addVideoStream(video,userVideoStream)
+            })
+        })
+    })
+function connectToNewUser(userId,stream){
+    const call = peer.call(userId,stream)
+    const video = document.createElement("video")
+    call.on("stream",(userVideoStream)=>{
+        addVideoStream(video,userVideoStream)
     })
 }
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+        video.play();
+        $("#video_grid").append(video)
+    });
+};
+
 $(function () {
     $("#show_chat").click(function () {
         $(".left-window").css("display", "none")
